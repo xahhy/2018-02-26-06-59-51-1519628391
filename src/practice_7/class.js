b6a8875bcbfea60210afab05d20419bcd0664a89
hoyhe <hoyhe@thoughtworks.com>
2018-09-06 18:46:06 +0800

    Finish practice_7

--- src/practice_7/class.js
+++ src/practice_7/class.js
@@ -1,1 +1,13 @@
 
+class Class {
+    constructor(number){
+        this.number = number;
+    }
+    getDisplayName(){
+        return `Class ${this.number}`;
+    }
+}
+
+export default Class;
+
+
