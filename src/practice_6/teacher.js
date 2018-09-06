936f3b68e2f05a3a7246c942f4056b7707d8bfd9
hoyhe <hoyhe@thoughtworks.com>
2018-09-06 18:38:41 +0800

    Finish practice_6

--- src/practice_6/teacher.js
+++ src/practice_6/teacher.js
@@ -1,1 +1,14 @@
 
+import Person from './person.js';
+
+class Teacher extends Person{
+    constructor(name, age, klass){
+        super(name, age);
+        this.klass = klass;
+    }
+
+    introduce(){
+        return `${super.introduce()} I am a Teacher. I teach ${this.klass ? 'Class '+this.klass : 'No Class'}.`
+    }
+}
+export default Teacher;
