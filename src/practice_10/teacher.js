ff7ca0163b843362555f8f03d5aef148a6d59848
hoyhe <hoyhe@thoughtworks.com>
2018-09-06 19:00:42 +0800

    Finish practice_10

--- src/practice_10/teacher.js
+++ src/practice_10/teacher.js
@@ -1,1 +1,23 @@
 
+import Person from './person.js';
+
+class Teacher extends Person{
+    constructor(id, name, age, klasses=[]){
+        super(id, name, age);
+        this.klasses = klasses;
+    }
+
+    introduce(){
+        let className = this.klasses.map(klasses=>klasses.number).join(', ');
+        return `${super.introduce()} I am a Teacher. I teach ${this.klasses.length ? 'Class '+ className : 'No Class'}.`
+    }
+
+    introduceWith(student){
+        if(this.klasses.number === student.klasses.number){
+            return `${super.introduce()} I am a Teacher. I teach ${student.name}.`
+        }else {
+            return `${super.introduce()} I am a Teacher. I don't teach ${student.name}.`
+        }
+    }
+}
+export default Teacher;
