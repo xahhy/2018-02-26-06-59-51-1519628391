0f8d621eb27c5579ebb0787ceec6587ce8ca253b
hoyhe <hoyhe@thoughtworks.com>
2018-09-06 19:22:41 +0800

    Finish practice_11

--- src/practice_11/class.js
+++ src/practice_11/class.js
@@ -1,1 +1,42 @@
+class Class {
+    constructor(number) {
+        this.leaderListeners = [];
+        this.assignListeners = [];
+        this.number = number;
+    }
+
+    getDisplayName() {
+        return `Class ${this.number}`;
+    }
+
+    assignLeader(student) {
+        if (student.klass.number === this.number) {
+            this.leader = student;
+            this.leaderListeners.forEach(teacher => teacher.notify(student));
+        }
+        else {
+            console.log('It is not one of us.');
+        }
+    }
+
+    appendMember(student) {
+        student.klass = this;
+        this.assignListeners.forEach(teacher=>teacher.notify(student))
+    }
+
+    isIn(student){
+        return student.klass.number === this.number;
+    }
+
+    registerAssignLeaderListener(teacher) {
+        this.leaderListeners.push(teacher);
+    }
+
+    registerJoinListener(teacher){
+        this.assignListeners.push(teacher);
+    }
+}
+
+export default Class;
+
 
--- src/practice_11/teacher.js
+++ src/practice_11/teacher.js
@@ -1,1 +1,35 @@
 
+import Person from './person.js';
+
+class Teacher extends Person{
+    constructor(id, name, age, klasses=[]){
+        super(id, name, age);
+        this.klasses = klasses;
+    }
+
+    introduce(){
+        let className = this.klasses.map(klasses=>klasses.number).join(', ');
+        return `${super.introduce()} I am a Teacher. I teach ${this.klasses.length ? 'Class '+ className : 'No Class'}.`
+    }
+
+    introduceWith(student){
+        if(this.klasses.number === student.klasses.number){
+            return `${super.introduce()} I am a Teacher. I teach ${student.name}.`
+        }else {
+            return `${super.introduce()} I am a Teacher. I don't teach ${student.name}.`
+        }
+    }
+
+    isTeaching(student){
+        return this.klass.filter(klass=>klass.isIn(student.klass)).length !== 0;
+    }
+
+    notify(student){
+        if(student.klass.leader && student.id === student.klass.leader.id){
+            console.log(`I am ${this.name}. I know ${student.name} become Leader of Class ${student.klass.number}.`);
+        }else{
+            console.log(`I am ${this.name}. I know ${student.name} has joined Class ${student.klass.number}.`);
+        }
+    }
+}
+export default Teacher;
